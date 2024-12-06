3b366e391da32bc4dc62baceef391a9fdbf3b162
ezawisza335 <ezawisza335@gmail.com>
2024-12-05 20:20:04 -0500

    my classmates are wizards

--- public/script.js
+++ public/script.js
@@ -1,1 +1,47 @@
-console.log('Hello World');
+const API = "http://localhost:8080/api/habits";
+
+//Get all habits and display them
+document
+  .getElementById("getHabits")
+  .addEventListener("click", async (event) => {
+    try {
+      event.preventDefault();
+      const response = await fetch(API);
+      const habits = await response.json();
+
+      const habitsList = document.getElementById("habits-list");
+      habitsList.innerHTML = "";
+
+      habits.forEach((habit) => {
+        const li = document.createElement("li");
+        li.innerHTML = habit.name;
+        habitsList.appendChild(li);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+// Create new Habit
+const form = document.getElementById("habit-form");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const newHabit = {
+    name: document.getElementById("habit-name").value,
+    description: document.getElementById("habit-description").value,
+    frequency: document.getElementById("habit-frequency").value,
+    completed: document.getElementById("habit-completed").value,
+  };
+
+  await fetch(API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newHabit),
+  });
+
+  form.reset();
+});
